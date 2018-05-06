4da4f7690de527666aef58cb6e7f816d2ddaea4d
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-06 23:05:51 +0000

    add interactions to cli

--- lib/best_novels/cli.rb
+++ lib/best_novels/cli.rb
@@ -1,5 +1,31 @@
 class BestNovels::CLI
   def call
-    puts "View The Guardian's Best Novels Ever Written"
+    list_novels
+    menu
+    goodbye
   end
+
+  def list_novels
+    puts "The Guardian's Best Novels Ever Written:"
+  end
+
+  def menu
+    puts "Enter the number of the novel you would to get more info on:"
+    input = gets.strip.downcase
+
+    if input.to_i > 0
+    puts "More info on novel"
+    elsif
+    input == "list"
+    list_novels
+    elsif
+    input == "exit"
+    goodbye
+    else
+    puts "Type Menu or Exit to leave program"
+  end
+ end
+
+def goodbye
+  puts "Goodbye"
 end
