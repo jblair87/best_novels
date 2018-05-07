6800c6128461b1009cdc3f034a36f28e0be4c08a
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-07 01:05:03 +0000

    added attributes to novel.rb and classes

--- lib/best_novels/novel.rb
+++ lib/best_novels/novel.rb
@@ -1,7 +1,21 @@
 class BestNovel::Novel
-  attr_accessor :title, :author, :description, :url
+  attr_accessor :title, :author, :year, :description, :url
+
+def initialize(title=nil, author=nil, year=nil)
+  @title = title
+  @author = author
+  @year = year
+  @@all << self
+end
 
 def self.all
+  @@all
+end
+
+def summary
+end
+
+def url
 end
 
 end
