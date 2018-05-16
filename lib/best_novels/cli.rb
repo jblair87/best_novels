1c93a52caaeb630ff2f53f435826b9954e64c03e
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-16 16:46:14 +0000

    combine messages, refer to scraper, change from name to number

--- lib/best_novels/cli.rb
+++ lib/best_novels/cli.rb
@@ -9,17 +9,18 @@ class BestNovels::CLI
     puts ""
       puts "The Guardian's Best Novels Ever Written:"
       puts ""
+      BestNovels::Scraper.scrape_best_novels
       BestNovels::Novel.all.each.with_index(1) do |novel, i|
-      puts "{i}. #{novel.title} - #{novel.author}"
+      puts "{i}. #{novel.title}"
   end
      end
 
 
 def print_novel(novel)
 puts ""
-puts "#{novel.title} - #{novel.author}"
+puts "#{novel.title}"
 puts ""
-puts novel.summary
+puts "#{novel.summary}"
 puts ""
 end
 
@@ -27,16 +28,13 @@ def start
     input = nil
     while input != "exit"
       puts ""
-       puts "What novel would you like more information on? Enter the title."
-      puts ""
-      puts "Enter list to see the list of novels again."
-      puts "Enter exit to leave the program."
+       puts "Enter the name of the novel you would like more information on, type 'list' to see the list of novels again or type 'exit' to leave the program"
       puts ""
     input = gets.strip.downcase
     if input == "list"
     list
   elsif input.to_i == 0
-    if novel = BestNovels::Novel.find_by_name(input)
+    if novel = BestNovels::Novel.find(input.to_i)
     print_novel(novel)
     end
   end
