ad16178c4bc03fcb8a5b6bb4ffdc6860e39a38e3
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-19 23:00:46 +0000

    scraped novels and fixed page

--- lib/best_novels/scraper.rb
+++ lib/best_novels/scraper.rb
@@ -1,13 +1,12 @@
-class BestNovels::Scraper
+class Scraper
 
 def self.scrape_best_novels
   doc = Nokogiri::HTML(open("https://www.theguardian.com/books/2015/aug/17/the-100-best-novels-written-in-english-the-full-list"))
-     doc.css("div.content__article-body p").each do |novel|
-        novel_info = {
-        :title =>  novel.css("u-underline strong a").text,
-    :summary => novel.css("p").text
-        }
-       novel_info
-      end
+  doc.css(".content__article-body.from-content-api.js-article__body p").each do |novel|
+  title = novel.css(".u-underline").text
+  url = novel.css("a").first.attr("href").strip
+  novels << {title: title,  url: url}
   end
+end
+novels
 end
