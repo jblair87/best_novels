f97e7629c9d094d31a8b7bbe6a746ce28a5bcddf
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-20 21:36:11 +0000

    fixed and updated scraper. still needs to be updated

--- lib/best_novels/scraper.rb
+++ lib/best_novels/scraper.rb
@@ -1,12 +1,13 @@
-class Scraper
+class BestNovels::Scraper
 
-def self.scrape_best_novels
-  doc = Nokogiri::HTML(open("https://www.theguardian.com/books/2015/aug/17/the-100-best-novels-written-in-english-the-full-list"))
-  doc.css(".content__article-body.from-content-api.js-article__body p").each do |novel|
-  title = novel.css(".u-underline").text
-  url = novel.css("a").first.attr("href").strip
-  novels << {title: title,  url: url}
+def self.scrape_list_page
+  list_page = Nokogiri::HTML(open("https://www.theguardian.com/books/2015/aug/17/the-100-best-novels-written-in-english-the-full-list"))
+  novels = []
+  list_page.css(".content__article-body.from-content-api.js-article__body p").each do |novel|
+  novel_title = novel.css(".u-underline ").text
+  novel_summary = novel.css("p")[2].text
+  novel_url = novel.css("a").first.attr("href").strip
+  novels << {title: novel_title, summary: novel_summary, url: novel_url}
   end
 end
-novels
 end
