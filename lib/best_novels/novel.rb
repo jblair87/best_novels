ae350ac39af7b83f4eea92f20f883d85325c8544
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-21 21:43:48 +0000

    add scraper for individual novel page and add description tto class and change url to novel url

--- lib/best_novels/novel.rb
+++ lib/best_novels/novel.rb
@@ -1,19 +1,33 @@
 class BestNovels::Novel
-  attr_accessor :title, :summary, :url
- @@all = []
+attr_accessor :title, :summary, :novel_url
 
-def initialize(title, summary, url)
+@@all = []
+
+def initialize(title = nil, summary = nil,  novel_url = nil)
   @title = title
   @summary = summary
-  @url = url
+  @novel_url = url
    @@all << self
 end
 
-  def self.all
-   @@all
-   end 
+def self.all
+    @@all
+  end
+
+  def self.all_clear
+    @@all.clear
+  end
+
 
   def self.find(input)
      self.all[input-1]
    end
+
+
+   def details
+       info = BestNovels::Scraper.scrape_novel_details
+       self.summary = info.values_at(:summary).join
+       puts "#{self.summary}"
+     end
+
 end
--- lib/best_novels/scraper.rb
+++ lib/best_novels/scraper.rb
@@ -1,13 +1,20 @@
 class BestNovels::Scraper
 
-def self.scrape_list_page
-  list_page = Nokogiri::HTML(open("https://www.theguardian.com/books/2015/aug/17/the-100-best-novels-written-in-english-the-full-list"))
-  novels = []
-  list_page.css(".content__article-body.from-content-api.js-article__body p").each do |novel|
-  novel_title = novel.css(".u-underline ").text
-  novel_summary = novel.css("p")[2].text
+def self.scrape_novels
+  doc = Nokogiri::HTML(open("https://www.theguardian.com/books/2015/aug/17/the-100-best-novels-written-in-english-the-full-list"))
+  doc.css(".content__article-body.from-content-api.js-article__body p").each do |novel|
+  novel_title = novel.css(".u-underline").text
   novel_url = novel.css("a").first.attr("href").strip
-  novels << {title: novel_title, summary: novel_summary, url: novel_url}
+  novels << {title: novel_title, novel_url: novel_url}
   end
 end
+
+
+def self.scrape_novel_details(url)
+  details = {}
+   doc = Nokogiri::HTML(open(url))
+   details[:summary] = doc.css("p").text
+   details
+  end
+
 end
