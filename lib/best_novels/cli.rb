c4e061c21480553e5197ceadc1d6eac0448bf134
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-22 06:54:09 +0000

    organized user input options, moved welcome message to start, separated novel details

--- lib/best_novels/cli.rb
+++ lib/best_novels/cli.rb
@@ -1,43 +1,51 @@
 class BestNovels::CLI
   def call
-    list
+    puts "Welcome to the 100 Best Novels Gem"
     start
   end
 
-
   def list
-    puts ""
+      puts ""
       puts "The Guardian's Best Novels Ever Written:"
       puts ""
-      BestNovels::Scraper.scrape_list_page
+      BestNovels::Scraper.scrape_novels
       BestNovels::Novel.all.each.with_index(1) do |novel, i|
       puts "{i}. #{novel.title}"
+    end
   end
-     end
 
-
-def print_novel(novel)
-puts ""
-puts "#{novel.title}"
-puts ""
-puts "#{novel.summary}"
-puts ""
-end
+   def print_novel(novel)
+     puts ""
+     puts "#{novel.title}"
+     puts ""
+     puts "#{novel.summary}"
+     puts ""
+     puts "Novel information found at #{novel.novel_url}"
+  end
 
 def start
     input = nil
     while input != "exit"
       puts ""
-       puts "Enter the number of the novel you would like more information on, type 'list' to see the list of novels again or type 'exit' to leave the program"
+      puts "Enter 'list' to see the list of novels"
+      puts "Enter 'more info' to learn more about a novel"
+      puts "Enter 'exit' to leave the program"
       puts ""
-    input = gets.strip
-    if input == "list"
-    list
-  elsif input.to_i > 0 
-    print_novel(novel)
+      input = gets.strip
+      if input == "list"
+      list
+     elsif input == "more info"
+          puts "What novel would you more information on?"
+                if input.to_i == 0
+                    novel = BestNovels::Novel.find_by_title(input)
+                    print_novel(novel)
+                else
+                  puts "Invalid Entry"
+                end
+    else input == "exit"
+    puts "Goodbye!!!"
+      exit
     end
- end
- puts "Goodbye!!!"
-
+  end
 end
 end
