972c86eb5aceb3137c4c91703db8b6c80fbc3158
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-22 23:06:20 +0000

    move url to index

--- lib/best_novels/scraper.rb
+++ lib/best_novels/scraper.rb
@@ -2,19 +2,18 @@ class BestNovels::Scraper
 
 def self.scrape_novels
   doc = Nokogiri::HTML(open("https://www.theguardian.com/books/2015/aug/17/the-100-best-novels-written-in-english-the-full-list"))
-  novels = []
   doc.css(".content__article-body.from-content-api.js-article__body p").each do |novel|
   title = novel.css(".u-underline").text
-  novels << {title: title}
+  novel_url = novel.css("a").attr("href").value
+  novel << {title: title, novel_url: novel_url}
   end
 end
 
 
-def self.scrape_novel_details(url)
+def self.scrape_novel_details(novel_url)
   details = {}
-   doc = Nokogiri::HTML(open(url))
-   details[:summary] = doc.css("p").text
-   novel_url[:novel_url] = doc.css("a").first.attr("href").strip
+   doc = Nokogiri::HTML(open(novel_url))
+   details[:summary] = doc.css(".content__article-body.from-content-api.js-article__body p").text
    details
   end
 
