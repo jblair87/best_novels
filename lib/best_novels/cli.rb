60ddc75f6e60c8121a7ac8727d85a049c9981e35
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-23 19:11:23 +0000

    added hash to i in list, changed number to word in start

--- lib/best_novels/cli.rb
+++ lib/best_novels/cli.rb
@@ -8,14 +8,15 @@ class BestNovels::CLI
       puts ""
       puts "The Guardian's Best Novels Ever Written:"
       puts ""
+      #BestNovels::Novel.destroy
       BestNovels::Scraper.scrape_novels
       BestNovels::Novel.all.each.with_index(1) do |novel, i|
-      puts "{i}. #{novel.title}"
+      puts "---- #{1}. #{novel.title} ---"
+      puts ""
     end
   end
 
    def print_novel(novel)
-     puts ""
      puts "#{novel.title}"
      puts ""
      puts "#{novel.summary}"
@@ -27,7 +28,7 @@ def start
     input = nil
     while input != "exit"
       puts ""
-      puts "Enter 'list' to see the list of novels"
+      puts "Enter 'list' to see the list of novels again"
       puts "Enter 'more info' to learn more about a novel"
       puts "Enter 'exit' to leave the program"
       puts ""
@@ -36,9 +37,10 @@ def start
       list
      elsif input == "more info"
           puts "What novel would you more information on?"
-                if input.to_i == 0
-                    novel = BestNovels::Novel.find_by_title(input)
-                    print_novel(novel)
+          input = gets.strip.downcase
+          novel = BestNovels::Novel.find(input.to_i)
+                if novel
+                print_novel(novel)
                 else
                   puts "Invalid Entry"
                 end
