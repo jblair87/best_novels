a6bb7413ea171ffe83f81932e933e8229e927d09
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-26 03:08:38 +0000

    change each to each with index, moved paragraph

--- lib/best_novels/scraper.rb
+++ lib/best_novels/scraper.rb
@@ -1,17 +1,17 @@
 class BestNovels::Scraper
 
-def self.scrape_novels
+def scrape_novels
   doc = Nokogiri::HTML(open("https://www.theguardian.com/books/2015/aug/17/the-100-best-novels-written-in-english-the-full-list"))
-  doc.css("#article .content__article-body.from-content-api.js-article__body p").each do |novel|
+  best_novels = doc.css(".content__article-body.from-content-api.js-article__body").to_a
+  best_novels.each.with_index do |novel, i|
   novel = BestNovels::Novel.new
-  novel.title = doc.css(".u-underline").text
-  novel.summary = doc.css("p[2]").text
-  novel.novel_url = doc.css("a").attr("href").value
+  novel.title = doc.css("p .u-underline")[i].text
+  novel.novel_url = doc.css("a")[i].attr("href")
+  novel.summary = doc.css("p")[i].text.gsub("/s"," ")
   novel.save
   #novel << {title: title, novel_url: novel_url}
   end
 end
-
 
 #def self.scrape_novel_details(novel_url)
 #  details = {}
