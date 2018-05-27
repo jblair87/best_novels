e68ebfdeec7b45969f70aa2eaddf1633ab372f30
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-27 22:00:23 +0000

    remove list, init, create and clean up page

--- lib/best_novels/novel.rb
+++ lib/best_novels/novel.rb
@@ -3,36 +3,21 @@ attr_accessor :title, :summary, :novel_url
 
 @@all = []
 
-def initialize(title = nil, summary = nil,  novel_url = nil)
-  @title = title
-  @summary = summary
-  @novel_url = novel_url
-   @@all << self
-end
-
-def self.create(novel_array)
-novel_array.each {|novel| self.new(novel)}
-end
-
-def self.list_novels
-@@all.each.with_index(1) {|novel, index| puts "#{index}. #{novel.title}"}
-end
-
-def save
-    @@all << self
-  end
 
 def self.all
     @@all
   end
 
-  def self.find(input)
-     self.all[input-1]
-   end
+def save
+  @@all << self
+end
 
-   def self.destroy
-      @@all = []
-    end
+def self.find(input)
+   self.all[input-1]
+end
 
+def self.destroy
+  @@all = []
+end
 
 end
