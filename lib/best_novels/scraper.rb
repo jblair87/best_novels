b5c8c098253d4769f7825c1b27060643966ce7c8
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-30 04:15:58 +0000

    add item prop scraper, added novel array, changed each with index to each

--- lib/best_novels/scraper.rb
+++ lib/best_novels/scraper.rb
@@ -1,16 +1,18 @@
 class BestNovels::Scraper
 
 def scrape_novels
+  novels = []
   doc = Nokogiri::HTML(open("https://www.theguardian.com/books/2015/aug/17/the-100-best-novels-written-in-english-the-full-list"))
-  best_novels = doc.css(".content__article-body.from-content-api.js-article__body").to_a
-  best_novels.each.with_index do |novel, i|
-  novel = BestNovels::Novel.new
-  novel.title = doc.css("p:nth-child(1) a.u-underline")[i].text
-  novel.novel_url = doc.css("a")[i].attr("href")
-  novel.summary = doc.css("div.content__main-column div.content__article-body p:nth-child(2)")[i].text.gsub("/t"," ").gsub("/n"," ")
-  novel.save
+  best_novels = doc.search("div[itemprop='articleBody']")
+  best_novels.each do |novel|
+  new_novel = BestNovels::Novel.new
+  new_novel.title = novel.css("p .u-underline").text
+  new_novel.novel_url = novel.css("a").attr("href")
+  new_novel.summary = novel.css("p:nth-child(2)").text.gsub("/t"," ").gsub("/n"," ")
+  novels << new_novel
   #novel << {title: title, novel_url: novel_url}
   end
+  novels
 end
 
 #def self.scrape_novel_details(novel_url)
