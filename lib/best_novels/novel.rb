a120b014969ab43b84f8a5162ee601da90a51e52
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-05-30 04:16:56 +0000

    add init change find input to id remove destroy

--- lib/best_novels/novel.rb
+++ lib/best_novels/novel.rb
@@ -3,6 +3,12 @@ attr_accessor :title, :summary, :novel_url
 
 @@all = []
 
+def initialize(title = nil, summary = nil,  novel_url = nil)
+  @title = title
+  @summary = summary
+  @novel_url = novel_url
+   @@all << self
+end
 
 def self.all
     @@all
@@ -12,12 +18,9 @@ def save
   @@all << self
 end
 
-def self.find(input)
-   self.all[input-1]
-end
+def self.find(id)
+   @@all[id.to_i-1]
+ end
 
-def self.destroy
-  @@all = []
-end
 
 end
