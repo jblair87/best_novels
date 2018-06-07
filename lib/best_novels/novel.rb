912cb06aaaa67e3b12337c2c1a7247cd0826ac48
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-06-07 00:32:33 +0000

    remove initalize

--- lib/best_novels/novel.rb
+++ lib/best_novels/novel.rb
@@ -3,12 +3,6 @@ attr_accessor :title, :summary, :novel_url
 
 @@all = []
 
-def initialize(title = nil, summary = nil,  novel_url = nil)
-  @title = title
-  @summary = summary
-  @novel_url = novel_url
-   @@all << self
-end
 
 def self.all
     @@all
@@ -19,7 +13,7 @@ def save
 end
 
 def self.find(id)
-   @@all[id.to_i-1]
+   self.all[id-1]
  end
 
 
