d34a2aa3c9daab6a6b13c62d04c19a74e33937a9
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-06-08 21:34:23 +0000

    change css selector p to nth of type from nth child

--- lib/best_novels/scraper.rb
+++ lib/best_novels/scraper.rb
@@ -2,11 +2,11 @@ class BestNovels::Scraper
 
 def scrape_novels
   doc = Nokogiri::HTML(open("https://www.theguardian.com/books/2015/aug/17/the-100-best-novels-written-in-english-the-full-list"))
-doc.search(".content__article-body").each do |new_novel|
+doc.css("div.content__article-body").each do |new_novel|
   novel = BestNovels::Novel.new
-  novel.title = new_novel.search("p:nth-child(1) strong a.u-underline").text.strip
-  novel.novel_url = new_novel.search("a.u-underline").attr("href").value
-  novel.summary = new_novel.search("p:nth-child(2)").text.gsub("/t"," ").gsub("/n"," ")
+  novel.title = new_novel.css("p:nth-of-type(1) a.u-underline").text
+  novel.novel_url = new_novel.css("p:nth-of-type(odd) strong a.u-underline").attr("href").value
+  novel.summary = new_novel.css("p:nth-of-type(even)").text.gsub("/t"," ").gsub("/n"," ")
   #novels << new_novel
   novel.save
   #novel << {title: title, novel_url: novel_url}
