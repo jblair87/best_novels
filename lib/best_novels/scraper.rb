3993d41b48680173a02e98d33844de006cc56f5b
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-08-08 19:10:20 +0000

    changed each to each with index, changed to doc.css, removed summary for now

--- lib/best_novels/scraper.rb
+++ lib/best_novels/scraper.rb
@@ -1,15 +1,17 @@
 class BestNovels::Scraper
 
-def scrape_novels
+def self.scrape_novels
   doc = Nokogiri::HTML(open("https://www.theguardian.com/books/2015/aug/17/the-100-best-novels-written-in-english-the-full-list"))
-doc.css("div.content__article-body").each do |new_novel|
+  content = doc.css("div.content__article-body").to_a
+  content.each.with_index do |novel, i|
   novel = BestNovels::Novel.new
-  novel.title = new_novel.css("p:nth-of-type(1) a.u-underline").text
-  novel.novel_url = new_novel.css("p:nth-of-type(odd) strong a.u-underline").attr("href").value
-  novel.summary = new_novel.css("p:nth-of-type(even)").text.gsub("/t"," ").gsub("/n"," ")
+  novel.title = doc.css("div.content__article-body a.u-underline")[i].text
+  novel.novel_url = doc.css("div.content__article-body strong a.u-underline")[i].attr("href")
+  #novel.summary = new_novel.css("p:nth-of-type(even)").text.gsub("/t", " ").gsub("/n", " ")
   #novels << new_novel
-  novel.save
+novel.save
   #novel << {title: title, novel_url: novel_url}
+end
 end
 #def self.scrape_novel_details(novel_url)
 #  details = {}
@@ -19,5 +21,4 @@ end
 #  summary = doc.css(".content__article-body.from-content-api.js-article__body p").text
 #   details
 #  end
-end
 end
