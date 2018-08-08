d1b748681b1b0be6afc9f7a20b8f3025a6ab0fa1
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-08-08 19:11:59 +0000

    moved scraper to list, aligned items

--- lib/best_novels/cli.rb
+++ lib/best_novels/cli.rb
@@ -1,19 +1,19 @@
 class BestNovels::CLI
   def call
-    BestNovels::Scraper.new.scrape_novels
     puts "Welcome to the 100 Best Novels Gem"
     start
   end
 
   def list
-      puts ""
-      puts "The Guardian's Best Novels Ever Written:"
-      puts ""
-      #BestNovels::Novel.destroy
-      BestNovels::Novel.all.each.with_index(1) do |novel, index|
-      puts "#{index}. #{novel.title}"
-end
-end
+        puts ""
+        puts "The Guardian's Best Novels Ever Written:"
+        puts ""
+        #BestNovels::Novel.destroy
+        BestNovels::Scraper.scrape_novels
+        BestNovels::Novel.all.each.with_index(1) do |novel, index|
+        puts "#{index}. #{novel.title}"
+   end
+   end
 
    def print_novel(novel)
      puts ""
@@ -24,25 +24,26 @@ end
      puts "Learn more at #{novel.novel_url}"
   end
 
-def start
-    input = nil
-    while input != "exit"
-      puts ""
-      puts "Enter 'list' to see the list of novels again"
-      puts "Enter the number '1-100' to learn more about a novel"
-      puts "Enter 'exit' to leave the program"
-      puts ""
-      input = gets.strip
+  def start
+    list
+      input = nil
+      while input != "exit"
+        puts ""
+        puts "Enter 'list' to see the list of novels again"
+        puts "Enter the number '1-100' to learn more about a novel"
+        puts "Enter 'exit' to leave the program"
+        puts ""
+        input = gets.strip
       if input == "list"
-      list
-    elsif input.to_i > 0
-              if novel = BestNovels::Novel.find(input.to_i)
-               print_novel(novel)
-           end
-else
-    puts "Goodbye!!!"
-
+        list
+      elsif input.to_i > 0
+          if novel = BestNovels::Novel.find(input.to_i)
+          print_novel(novel)
+          end
+      else
+      puts "Goodbye!!!"
+      end
     end
   end
-end
+
 end
